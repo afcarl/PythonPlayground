a8b1a59b28c78a317e16076f77fb57e9086f2587
tarunchhabra26 <tarun.chhabra@gmail.com>
2016-09-05 20:49:03 -0400

    Python is sorted method

---
+++ IsSorted/is_sorted.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+"""
+Write a function called is_sorted that takes a list as a parameter and returns True
+if the list is sorted in ascending order and False otherwise. You can assume (as a precondition) that
+the elements of the list can be compared with the relational operators <, >, etc.
+"""
+
+__author__ = "Tarun Chhabra"
+__copyright__ = "Copyright 2016"
+__license__ = "MIT"
+__version__ = "2.0"
+__maintainer__ = "Tarun Chhabra"
+__status__ = "Development"
+
+
+def is_sorted(input_list):
+    orig = input_list
+    if orig == sorted(input_list):
+        return True
+    else:
+        return False
+
+
+
+# Test Cases
+
+A = [11,8,5,6,10]
+
+print is_sorted(A)
+
+B = [1,2,3,4,5]
+
+print is_sorted(B)
